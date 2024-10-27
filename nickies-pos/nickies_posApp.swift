e7e840fccc476b6fb13fa83fedb74249d5cb5ae9
Nik Uzair <nikuzairsc@gmail.com>
2024-10-27 14:19:11 +0800

    Initial Commit

---
+++ nickies-pos/nickies_posApp.swift
@@ -0,0 +1,17 @@
+//
+//  nickies_posApp.swift
+//  nickies-pos
+//
+//  Created by Nik Uzair on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct nickies_posApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
